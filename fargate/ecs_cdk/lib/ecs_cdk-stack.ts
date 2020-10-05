0c489164fa3a6ad90d8e2cc1ca3f6f1ffa04ed91
Luis Morales <moralesl@users.noreply.github.com>
2020-10-05 11:40:52 +0200

    Unified CDK constructss code style
    Changes applied:
    * Use `import` instead of `require` statements
    * Use double quotes instead of single quotes
    * Use spacing around imports and object literals
    
    This increases the readability of the code

--- fargate/ecs_cdk/lib/ecs_cdk-stack.ts
+++ fargate/ecs_cdk/lib/ecs_cdk-stack.ts
@@ -1,10 +1,9 @@
-import cdk = require('@aws-cdk/core');
-
-import ec2 = require("@aws-cdk/aws-ec2");
-import ecs = require("@aws-cdk/aws-ecs");
-import dynamodb = require('@aws-cdk/aws-dynamodb');
-import ecs_patterns = require("@aws-cdk/aws-ecs-patterns");
-import iam = require("@aws-cdk/aws-iam");
+import * as cdk from "@aws-cdk/core";
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as ecs from "@aws-cdk/aws-ecs";
+import * as dynamodb from "@aws-cdk/aws-dynamodb";
+import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
+import * as iam from "@aws-cdk/aws-iam";
 
 class EcsCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -13,10 +12,10 @@ class EcsCdkStack extends cdk.Stack {
     const vpc = new ec2.Vpc(this, "quarkus-demo-vpc", {
       maxAzs: 3
     });
-    
-    const table = new dynamodb.Table(this, 'Users', {
-      partitionKey: { name: 'userId', type: dynamodb.AttributeType.STRING, },
-      tableName: 'Users',
+
+    const table = new dynamodb.Table(this, "Users", {
+      partitionKey: { name: "userId", type: dynamodb.AttributeType.STRING, },
+      tableName: "Users",
       readCapacity: 1,
       writeCapacity: 1,
       removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT recommended for production code
@@ -25,27 +24,27 @@ class EcsCdkStack extends cdk.Stack {
     const cluster = new ecs.Cluster(this, "quarkus-demo-cluster", {
       vpc: vpc
     });
-    
+
     const logging = new ecs.AwsLogDriver({
       streamPrefix: "quarkus-demo"
     })
 
-    const taskRole = new iam.Role(this, 'quarkus-demo-taskRole', {
-      roleName: 'quarkus-demo-taskRole',
-      assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com')
+    const taskRole = new iam.Role(this, "quarkus-demo-taskRole", {
+      roleName: "quarkus-demo-taskRole",
+      assumedBy: new iam.ServicePrincipal("ecs-tasks.amazonaws.com")
     });
-    
+
     const taskDef = new ecs.FargateTaskDefinition(this, "quarkus-demo-taskdef", {
       taskRole: taskRole
     });
-    
-    const container = taskDef.addContainer('quarkus-demo-web', {
+
+    const container = taskDef.addContainer("quarkus-demo-web", {
       image: ecs.ContainerImage.fromRegistry("smoell/quarkus_ecs_demo:0.7"),
       memoryLimitMiB: 256,
       cpu: 256,
       logging
     });
-    
+
     container.addPortMappings({
       containerPort: 8080,
       hostPort: 8080,
@@ -61,20 +60,20 @@ class EcsCdkStack extends cdk.Stack {
     });
 
     const scaling = fargateService.service.autoScaleTaskCount({ maxCapacity: 6 });
-    scaling.scaleOnCpuUtilization('CpuScaling', {
+    scaling.scaleOnCpuUtilization("CpuScaling", {
       targetUtilizationPercent: 10,
       scaleInCooldown: cdk.Duration.seconds(60),
       scaleOutCooldown: cdk.Duration.seconds(60)
     });
-    
+
     table.grantReadWriteData(taskRole)
-    
-    new cdk.CfnOutput(this, 'LoadBalancerDNS', { value: fargateService.loadBalancer.loadBalancerDnsName });
+
+    new cdk.CfnOutput(this, "LoadBalancerDNS", { value: fargateService.loadBalancer.loadBalancerDnsName });
   }
 }
 
 const app = new cdk.App();
-new EcsCdkStack(app, 'EcsCdkStack', {
+new EcsCdkStack(app, "EcsCdkStack", {
   env: {
     region: "us-east-1",
     account: process.env.CDK_DEFAULT_ACCOUNT,
